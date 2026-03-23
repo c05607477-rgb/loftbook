72ff2198015cc340b428d9847ee097cf263c49b0
c05607477-rgb <c05607477@gmail.com>
2026-03-23 16:14:19 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LoftBookApp from './LoftBookApp.jsx'
+import LoftBookApp from './LoftBookApp'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<LoftBookApp />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LoftBookApp />
+  </React.StrictMode>
+)
